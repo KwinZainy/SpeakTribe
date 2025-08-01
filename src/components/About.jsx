03c6bcf1c6a06f2778f98714aec575b8fa722299
KwinZainy <gbadamosiblessing651@gmail.com>
2025-08-01 18:27:09 +0100

    initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <section>
+            <h1 className="text-4xl md:text-6xl font-bold bg-gradient-to-r from-[#C0CA33] via-[#009688] to-[#1B5E20] text-transparent bg-clip-text animate-fadeIn">Speak Proud. Speak Native. SpeakTribe.</h1>
+             <p></p>
+        </section>
+        <section></section>
+
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { FaHeadSideCough } from "react-icons/fa6";
+const Hero = () => {
+  return (
+    <div className=' flex justify-center py-24 max-tablet:flex-col-reverse max-w-[1800px] m-auto min-desktop:justify-center'>
+        <section className=' w-[50%] flex flex-col gap-2 justify-center
+         max-tablet:items-center max-tablet:w-full max-tablet:mt-6 '> 
+          <h1 className=' text-[#263238] text-[40px] font-bold max-mobile:text-[23px] mx-auto min-desktop:text-[55px] min-desktop:text-center'> “Your Language. Your Power. <span className=' items-baseline inline-block text-[#009688]'><FaHeadSideCough/></span>”</h1>
+          <p className=' w-[72%] pl-5 items-center p-3 text-[20px] text-[#263238] text-center font-medium max-tablet:pl-2 max-mobile:w-[88%] max-mobile:text-[16px] mx-auto min-desktop:text-[28px] min-desktop:w-[90%]'>Yoruba, Igbo, Hausa — SpeakTribe brings your heritage to life with audio learning, cultural phrases, and real conversations.</p>
+          <div className=' flex gap-4 mx-auto max-tablet:pl-4 max-tablet:mt-3 '>
+            <button className=' bg-[#009688] text-[18px] font-light p-3 hover:bg-[#388E3C] text-white hover:rounded-[10px] max-mobile:text-[14px] min-desktop:text-[23px]'> Begin Your Journey</button>
+            <button className=' bg-[#009688] text-[18px] font-light p-3 hover:bg-[#388E3C] text-white hover:rounded-[10px] max-mobile:text-[14px] min-desktop:text-[23px]'> Already have an account</button>
+          </div>
+        </section>
+        <section >
+          <img src="./tribe.jpg" alt=""  className='w-[500px] max-tablet:w-full p-5 ' />
+          
+        </section>
+    </div>
+  )
+}
+
+export default Hero
