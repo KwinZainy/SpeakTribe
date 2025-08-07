6cfd7abdcc743ea6cd83dfaf70c57564458c1965
KwinZainy <gbadamosiblessing651@gmail.com>
2025-08-07 08:10:41 +0100

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Landing from './pages/Landing'
 import Footer from './static/Footer'
-
+import HomePage from './pages/HomePage'
 
 const App = () => {
   return (
     <div>
-      <Landing/>
+      <HomePage/>
      <Footer/>
     </div>
   )
--- src/static/Footer.jsx
+++ src/static/Footer.jsx
@@ -4,24 +4,24 @@ import { FaFacebook } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa6";
 const Footer = () => {
   return (
-    <div className=' bg-red-400 py-10'>
-      <section className=' flex justify-around mb-10'>
-      <p>About us</p>
-      <p>Contact</p>
-      <p>Privacy Policy</p>
-      <p>Terms of service</p>
+    <div className=' bg-[#009688] py-10 max-mobile: pl-4'>
+      <section className=' flex justify-around mb-10 max-mobile:flex-col max-mobile:gap-3.5'>
+      <p className=' text-white max-mobile:text-14px'>About us</p>
+      <p className=' text-white max-mobile:text-14px'>Contact</p>
+      <p className=' text-white max-mobile:text-14px'>Privacy Policy</p>
+      <p className=' text-white max-mobile:text-14px'>Terms of service</p>
       </section>
-      <div className=' flex justify-around items-center px-10 pb-10'>
-         <section className=' '>
-        <p>@ 2025 SpeakTribe. All rights reserved.</p>
+        <section className=' flex justify-center gap-10 pb-10 max-mobile:justify-start'>
+        <FaXTwitter className=' text-white'/>
+        <FaFacebook className=' text-white'/>
+        <FaInstagram className=' text-white'/>
       </section>
-      <section className=' flex justify-center gap-7 '>
-        <FaXTwitter/>
-        <FaFacebook/>
-        <FaInstagram/>
+
+         <section className='text-center max-mobile:text-green-900'>
+        <p className=' text-white  max-mobile:text-start max-mobile:text-[14px]'>@ 2025 SpeakTribe. All rights reserved.</p>
       </section>
+    
      
-      </div>
     </div>
   )
 }
