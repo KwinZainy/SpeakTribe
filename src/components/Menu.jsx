c994d72d3a2efb6cb046670f87cadc5c17b79442
KwinZainy <gbadamosiblessing651@gmail.com>
2025-08-10 20:30:54 +0100

    nav

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -10,10 +10,10 @@ const Menu = () => {
                </div>
          <div >
          {drop&&(
-           <div  className=' bg-white w-1/2 flex flex-col gap-12 items-end  h-dvh py-10 fixed top-0 right-0  z-1000 overflow-y-hidden'> 
+           <div  className=' bg-white w-full flex flex-col gap-12 items-end  h-dvh py-10 fixed top-0 right-0  z-1000 overflow-y-hidden'> 
                <div className=' flex items-center max-mobile:gap-40  max-tablet:gap-90'>
-                  <div onClick={(()=>setDrop(!drop))} className=' left-0'>
-              {drop? <ImCancelCircle size={35}/>: <IoPersonCircleSharp size={40} />} 
+                  <div onClick={(()=>setDrop(!drop))} className=' '>
+              {drop? <ImCancelCircle size={35} className=' left-'/>: <IoPersonCircleSharp size={40} />} 
                </div>
                </div>
                <div className='flex flex-col gap-4 mx-auto  max-tablet:mt-3 max-mobile:gap-2 '>
